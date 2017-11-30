74f243210a990e0bbfce67a8723e77f2261dbdd6
github username <github email address>
2017-11-30 10:38:24 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+
+# shows everyone their current place in line
+#if no one in line, say the line is empty
+
+def line(katz_deli_line)
+if katz_deli_line.length == 0
+  puts "The line is currently empty."
+else
+  greeting = "The line is currently: "
+  katz_deli_line.each_with_index do | name, pos |
+  greeting += "#{pos + 1}. #{name} "
+end
+formal_greeting = greeting.slice(0, (greeting.length-1))
+puts "#{formal_greeting}"
+end
+end
+
+def take_a_number(katz_deli_line, name)
+  katz_deli_line.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli_line.length} in line."
+end
+
+
+def now_serving(katz_deli_line)
+  if katz_deli_line.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli_line.shift}."
+  end
+end
