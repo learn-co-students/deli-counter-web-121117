ab875a923956dab57afc9bdf1cc1153f3e93d9ba
Julien Tregoat <jtregoatlearn@gmail.com>
2017-12-03 20:25:14 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+katz_deli = []
+
+def line(arr)
+  phrase = "The line is currently:"
+  if arr.size == 0
+    puts "The line is currently empty."
+  else
+    arr.each_with_index do |name, index|
+      place = index + 1
+      phrase = phrase + " " + place.to_s + ". " + name
+    end
+    puts phrase
+  end
+end
+
+def take_a_number(arr, newbie)
+  arr.push(newbie)
+  place = arr.size
+  puts "Welcome, #{newbie}. You are number #{place} in line."
+end
+
+def now_serving(arr)
+  if arr.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{arr.shift}."
+  end
+end
